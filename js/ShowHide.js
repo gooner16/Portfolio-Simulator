319ea95d84c9dca1fb5a739f67c215f4dfa92700
GitHubWhatIsIt <richardrencn@163.com>
2015-05-06 14:55:47 -0400

    Trying to debug

--- js/ShowHide.js
+++ js/ShowHide.js
@@ -1,18 +1,16 @@
 'use strict'; 
+var divStem = document.getElementById('Stem');
+var divPara = document.getElementById('Parameters');
+var divSim = document.getElementById('Simulator');
 
-function hideStem() {
+function setPara() {
 	var divStem = document.getElementById('Stem');
 	var divPara = document.getElementById('Parameters');
-	if (divStem.style.display == 'block') {
-		divStem.style.display = 'none';
-		divPara.style.display = 'block';
-	}
-	else {
-		divStem.style.display = 'block';
-	}
+		
+	divStem.style.display = 'none';
+	divPara.style.display = 'block';
 }
 
 function showSim() {
-	var divPara = document.getElementById('Simulator');
-		divPara.style.display = 'block';
+	divSim.style.display = 'block';
 }
