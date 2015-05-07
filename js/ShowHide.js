62ebff30f88cf6dc6c78f26193c0aba58206e304
GitHubWhatIsIt <richardrencn@163.com>
2015-05-07 14:35:51 -0400

    Program flow is in ShowHide is done
    Julian says we can store data into static variables for now so that it
    doesn't change when the browser is refreshed.

--- js/ShowHide.js
+++ js/ShowHide.js
@@ -1,16 +1,36 @@
 'use strict'; 
-var divStem = document.getElementById('Stem');
-var divPara = document.getElementById('Parameters');
-var divSim = document.getElementById('Simulator');
+var index = -1
 
-function setPara() {
+function showWindow(index) {
+	//Refer to different windows (divs)
 	var divStem = document.getElementById('Stem');
 	var divPara = document.getElementById('Parameters');
-		
-	divStem.style.display = 'none';
-	divPara.style.display = 'block';
-}
+	var divSim = document.getElementById('Simulator');
+	var divPopTrade = document.getElementById('Trade');
+	var divPopMarket = document.getElementById('Market');
 
-function showSim() {
-	divSim.style.display = 'block';
+	//Hide every window first
+	divStem.style.display = 'none';
+	divPara.style.display = 'none';
+	divSim.style.display = 'none';
+	divPopTrade.style.display = 'none';
+	divPopMarket.style.display = 'none';
+
+	//Show specific window
+	if (index == 0) {
+		divStem.style.display = 'block';
+	};
+	if (index == 1) {
+		divPara.style.display = 'block';
+	};
+	if (index == 2) {
+		divSim.style.display = 'block';
+	};
+	if (index == 3) {
+		divPopTrade.style.display = 'block';
+	};
+	if (index == 4) {
+		divPopMarket.style.display = 'block';
+	};
+
 }
