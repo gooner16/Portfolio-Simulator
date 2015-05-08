70feaaf1cd16d4bb35cfe2be34a089ffc7ff17e7
gooner16 <lukec978@gmail.com>
2015-05-08 09:12:30 -0400

    Program flow

--- js/ShowHide.js
+++ js/ShowHide.js
@@ -1,17 +1,49 @@
 function hideStem() {
     var hide = document.getElementById('Stem');
     var show = document.getElementById('Parameters');
-    if (hide.style.display !== 'none') {
         hide.style.display = 'none';
         show.style.display = 'block';
-    }
-    else {
-        hide.style.display = 'block';
-    }
 };
 
 function showSim() {
+	var hide = document.getElementById('Parameters');
     var show = document.getElementById('Simulator');
+        hide.style.display = 'none';
         show.style.display = 'block';
-
 };
+
+function showTrade() {
+	var hide = document.getElementById('Simulator');
+    var show = document.getElementById('Trade');
+        hide.style.display = 'none';
+        show.style.display = 'block';
+};
+
+function hideTrade() {
+	var hide = document.getElementById('Trade');
+    var show = document.getElementById('Simulator');
+        hide.style.display = 'none';
+        show.style.display = 'block';
+};
+
+function showPortfolios() {
+	var hide = document.getElementById('Simulator');
+    var show = document.getElementById('Stem');
+        hide.style.display = 'none';
+        show.style.display = 'block';
+};
+
+function showMarket() {
+	var hide = document.getElementById('Simulator');
+    var show = document.getElementById('Market');
+        hide.style.display = 'none';
+        show.style.display = 'block';
+};
+
+function hideMarket() {
+	var hide = document.getElementById('Market');
+    var show = document.getElementById('Simulator');
+        hide.style.display = 'none';
+        show.style.display = 'block';
+};
+
