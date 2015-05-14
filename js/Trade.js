37180669df4553a3e1b93f6d79913066e07e6c56
gooner16 <lukec978@gmail.com>
2015-05-13 22:18:33 -0400

    Add pricedata search and trade functions
    Trade.js - sellStock, buyStock
    Main.js - pull price data

--- js/Trade.js
+++ js/Trade.js
@@ -1,1 +1,38 @@
-'use strict'; 
+'use strict';
+
+function buyStock (ind) {
+
+	var amount = document.getElementById("amount"); //get amount input
+	var portId = "Port" + ind						//get current porfolio id
+	var port = document.getElementById(portId)
+
+	//Transaction calculations
+
+	if (Port0.commis > 1) {
+		buyPrice = (currentPrice * amount) + Port0.commis;
+	}else{
+		buyPrice = (currentPrice * amount) + (Port0.commission(currentPrice * amount));
+	};
+	
+	port.cash = port.cash - buyPrice;
+
+}
+
+function sellStock (ind) {
+	
+	var amount = document.getElementById("amount"); //get amount input
+	var portId = "Port" + ind						//get current portfolio id
+	var port = document.getElementById(portId)
+
+//Transaction calculations
+
+	if (Port0.commis > 1) {
+		sellPrice = (currentPrice * amount) - Port0.commis;
+	}else{
+		sellPrice = (currentPrice * amount) - (Port0.commission(currentPrice * amount));
+	};
+
+	
+	port.cash = port.cash + sellPrice;
+
+} 
